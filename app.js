29850004d409d45ca168d70efb3f68fc6237aaa6
Kai Duan <kai.duan.com@gmail.com>
2016-02-21 20:37:08 +0800

    init log

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const port = 3000
+
+
+app.get('/', function(req, res)
+{
+  res.sendFile(path.join(__dirname, './client/index.html'))
+})
+
+
+app.listen(port, function(err)
+{
+  if (err)
+  {
+    throw new Error(err)
+  }
+  console.log("Server is listening on:" + port)
+})
